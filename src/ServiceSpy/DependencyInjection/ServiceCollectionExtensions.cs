092dfaa4c621f1a05970d8fdf5a7a27dec1d4c07
Jeff Johnson <jjxtra@gmail.com>
2022-02-16 15:41:04 -0700

    New config classes

--- src/ServiceSpy/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/ServiceSpy/DependencyInjection/ServiceCollectionExtensions.cs
@@ -10,44 +10,20 @@ namespace ServiceSpy.DependencyInjection;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// Udp send/receive configuration
-    /// </summary>
-    internal class ServiceSpyUdpConfiguration
-    {
-        /// <summary>
-        /// IP address to bind to, empty string or * for auto
-        /// </summary>
-        public string IPAddress { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Port to send/receive on, default 52661
-        /// </summary>
-        public int Port { get; set; } = 52661;
-
-        /// <summary>
-        /// Whether to broadcast service info or only receive it
-        /// </summary>
-        public bool ReadOnly { get; set; }
-
-        /// <summary>
-        /// Services
-        /// </summary>
-        public ServiceMetadata[] Services { get; set; } = Array.Empty<ServiceMetadata>();
-    }
-
-    /// <summary>
     /// Add udp send/receive capabilities for service discovery.
     /// </summary>
     /// <param name="builder">Web application builder</param>
     /// <param name="configPath">Config key/path, default is ServiceSpy:Udp</param>
     /// <returns>Services</returns>
-    public static IServiceCollection AddServiceSpyUdp(this WebApplicationBuilder builder, string configPath = "ServiceSpy:Udp")
+    public static IServiceCollection AddServiceSpyUdp(this WebApplicationBuilder builder, string configPath = "ServiceSpy")
     {
+        /*
         var udpConfig = new ServiceSpyUdpConfiguration();
         builder.Configuration.Bind(configPath, udpConfig);
         IPEndPoint endPoint = new(System.Net.IPAddress.Parse(udpConfig.IPAddress), udpConfig.Port);
         builder.Services.AddSingleton<INotificationReceiver>(provider => new UdpNotificationReceiver(endPoint, provider.GetRequiredService<ILogger<UdpNotificationReceiver>>()));
         builder.Services.AddSingleton<INotificationSender>(provider => new UdpNotificationSender(endPoint, provider.GetRequiredService<ILogger<UdpNotificationSender>>()));
+        */
         return builder.Services;
     }
 }
---
+++ src/ServiceSpy/DependencyInjection/ServiceSpyConfiguration.cs
@@ -0,0 +1,92 @@
+﻿namespace ServiceSpy.DependencyInjection;
+
+/// <summary>
+/// Configuration options for serivce spy
+/// </summary>
+public class ServiceSpyConfiguration
+{
+    /// <summary>
+    /// Services
+    /// </summary>
+    public ServiceSpyServices Services { get; set; } = new();
+
+    /// <summary>
+    /// Health checks
+    /// </summary>
+    public ServiceSpyHealthChecks HealthChecks { get; set; } = new();
+
+    /// <summary>
+    /// Notifications
+    /// </summary>
+    public ServiceSpyNotifications Notifications { get; set; } = new();
+}
+
+/// <summary>
+/// Service spy services configuration
+/// </summary>
+public class ServiceSpyServices
+{
+    /// <summary>
+    /// Storage provider
+    /// </summary>
+    public string Storage { get; set; } = "InMemory";
+
+    /// <summary>
+    /// Services
+    /// </summary>
+    public IReadOnlyCollection<ServiceMetadata> Items { get; set; } = Array.Empty<ServiceMetadata>();
+}
+
+/// <summary>
+/// Service spy health checks configuration
+/// </summary>
+public class ServiceSpyHealthChecks
+{   
+    /// <summary>
+    /// Storage provider
+    /// </summary>
+    public string Storage { get; set; } = "InMemory";
+
+    /// <summary>
+    /// Whether to perform health checks (true) or only receive health check results (false)
+    /// </summary>
+    public bool PerformHealthChecks { get; set; }
+}
+
+/// <summary>
+/// Service spy notifications configuration
+/// </summary>
+public class ServiceSpyNotifications
+{
+    /// <summary>
+    /// Whether to both receive and broadcast notifications (true) or to only receive notifications (false).
+    /// Note- health checks will always be broadcast if health check PerformHealthChecks is true.
+    /// </summary>
+    public bool Broadcast { get; set; }
+
+    /// <summary>
+    /// Connection info
+    /// </summary>
+    public ServiceSpyConnection Connection { get; set; } = new();
+}
+
+/// <summary>
+/// Service spy connection details
+/// </summary>
+public class ServiceSpyConnection
+{
+    /// <summary>
+    /// Protocol of connection, default is Udp
+    /// </summary>
+    public string Protocol { get; set; } = "Udp";
+
+    /// <summary>
+    /// IP address
+    /// </summary>
+    public string IPAddress { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Port
+    /// </summary>
+    public int Port { get; set; }
+}
